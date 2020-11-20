129701078b8ea23497e5b77a916688d672cd3fbc
KlasJoensson <klas.joensson@gmail.com>
2020-11-20 18:22:51 +0100

    Implements WorkItem

--- SpringPhotoAnalyzer/src/main/java/com/example/SpringPhotoAnalyzer/WorkItem.java
+++ SpringPhotoAnalyzer/src/main/java/com/example/SpringPhotoAnalyzer/WorkItem.java
@@ -2,8 +2,35 @@ package com.example.SpringPhotoAnalyzer;
 
 /**
  * Used as a model that stores Amazon Recognition data.
- * TODO Implement
  */
 public class WorkItem {
 
+	 private String key;
+	 private String name;
+	 private String confidence ;
+
+	 public void setKey (String key) {
+	    this.key = key;
+	 }
+
+	 public String getKey() {
+	    return this.key;
+	 }
+
+	 public void setName (String name) {
+	    this.name = name;
+	 }
+
+	 public String getName() {
+	    return this.name;
+	 }
+
+	 public void setConfidence (String confidence) {
+	    this.confidence = confidence;
+	 }
+
+	 public String getConfidence() {
+	    return this.confidence;
+	  }
+	 
 }
