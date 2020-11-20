ca4ca5acbc53497a0f9f1593c0f15840ee3f30df
KlasJoensson <klas.joensson@gmail.com>
2020-11-20 16:59:49 +0100

    Implements BucketItem

--- SpringPhotoAnalyzer/src/main/java/com/example/SpringPhotoAnalyzer/BucketItem.java
+++ SpringPhotoAnalyzer/src/main/java/com/example/SpringPhotoAnalyzer/BucketItem.java
@@ -2,8 +2,43 @@ package com.example.SpringPhotoAnalyzer;
 
 /**
  * Used as a model that stores Amazon S3 bucket information.
- * TODO Implement
  */
 public class BucketItem {
+	
+	private String key;
+	private String owner;
+	private String date;
+	private String size;
 
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getSize() {
+		return this.size;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getOwner() {
+		return this.owner;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return this.key;
+	}
 }
