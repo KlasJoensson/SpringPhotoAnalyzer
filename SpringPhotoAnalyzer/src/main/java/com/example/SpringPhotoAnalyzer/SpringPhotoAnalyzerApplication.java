e7fdeac481720ee99f434952f8a46807e89f509a
KlasJoensson <klas.joensson@gmail.com>
2020-11-20 13:45:08 +0100

    Init project

---
+++ SpringPhotoAnalyzer/src/main/java/com/example/SpringPhotoAnalyzer/SpringPhotoAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPhotoAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhotoAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPhotoAnalyzerApplication.class, args);
+	}
+
+}
---
+++ SpringPhotoAnalyzer/src/test/java/com/example/SpringPhotoAnalyzer/SpringPhotoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPhotoAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPhotoAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
